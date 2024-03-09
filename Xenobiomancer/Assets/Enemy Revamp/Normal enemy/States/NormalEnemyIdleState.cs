f5cc11122c9e3c0aa22911f8d03c385b4003ad1b
AsrielAcocella <criticalcss1917@gmail.com>
2024-03-10 00:42:26 +0800

    Update EnemySprite

---
+++ Xenobiomancer/Assets/Enemy Revamp/Normal enemy/States/NormalEnemyIdleState.cs
@@ -0,0 +1,35 @@
+﻿using Patterns;
+using System.Collections;
+using UnityEngine;
+
+namespace enemyT
+{
+    public class NormalEnemyIdleState : BasicIdleEnemyState
+    {
+        private Animator animator;
+
+        public Animator Animator { get { return animator; } }
+        public NormalEnemyIdleState(FSM fsm, EnemyBase enemy) : base(fsm, enemy)
+        {
+        }
+
+        protected override void DecideNextState()
+        {
+            if (PlayerWithinVision() || enemyReference.Path != null || PlayerWithinSenseRange())
+            {
+                mFsm.SetCurrentState((int)EnemyState.CHASING);
+            }
+            else
+            {
+                mFsm.SetCurrentState((int)(EnemyState.IDLE));
+            }
+        }
+
+        public override void Update()
+        {
+            animator.SetFloat("Pos X", 0);
+            animator.SetFloat("Pos Y", 0);
+            base.Update();
+        }
+    }
+}
